fa49a8897304544cf3b61d97babab2c27b2c43f6
datadave <605409+datadavev@users.noreply.github.com>
2024-03-06 10:37:54 -0500

    update dependencies

--- n2t/app.py
+++ n2t/app.py
@@ -1,43 +1,113 @@
+import contextlib
+import functools
+import typing
+
 import fastapi
 import fastapi.staticfiles
 import fastapi.templating
-
+import sqlalchemy.orm
 import rslv.routers.resolver
 
-from .config import settings
+from .config import get_settings
 from . import __version__
 
-app = fastapi.FastAPI(
-    title="N2T",
-    description=__doc__,
-    version=__version__,
-    contact={"name": "Dave Vieglais", "url": "https://github.com/datadavev/"},
-    license_info={
-        "name": "MIT",
-        "url": "https://opensource.org/license/mit/",
-    },
-    # Need to add this here because adding to the router has no effect.
-    lifespan=rslv.routers.resolver.resolver_lifespan,
-    openapi_url="/api/v1/openapi.json",
-    docs_url="/api",
-)
 
-app.mount(
-    "/static",
-    fastapi.staticfiles.StaticFiles(directory=settings.static_dir),
-    name="static",
-)
+@functools.lru_cache(maxsize=None)
+def get_engine(dbcnstr: str) -> sqlalchemy.engine.base.Engine:
+    return sqlalchemy.create_engine(dbcnstr, pool_pre_ping=True)
 
 
-templates = fastapi.templating.Jinja2Templates(
-    directory=settings.template_dir
-)
-@app.get("/favicon.ico", include_in_schema=False)
-async def get_favicon():
-    raise fastapi.HTTPException(status_code=404, detail="Not found")
+@contextlib.contextmanager
+def get_dbsession(dbengine) -> typing.Iterator[sqlalchemy.orm.Session]:
+    dbsession = sqlalchemy.orm.sessionmaker(bind=dbengine)()
+    try:
+        yield dbsession
+    except Exception:
+        dbsession.rollback()
+        raise
+    finally:
+        dbsession.close()
 
-@app.get("/", include_in_schema=False)
-async def redirect_docs(request:fastapi.Request):
-    return templates.TemplateResponse("index.html", {"request": request})
 
-app.include_router(rslv.routers.resolver.router)
+def create_application()->fastapi.FastAPI:
+
+    @contextlib.asynccontextmanager
+    async def dbengine_lifespan(app: fastapi):
+        dbcnstr = app.state.settings.db_connection_string
+        app.state.dbengine = get_engine(dbcnstr)
+        yield
+        if app.state.dbengine is not None:
+            app.state.dbengine.dispose()
+
+
+
+    app = fastapi.FastAPI(
+        title="N2T",
+        description=__doc__,
+        version=__version__,
+        contact={"name": "Dave Vieglais", "url": "https://github.com/datadavev/"},
+        license_info={
+            "name": "MIT",
+            "url": "https://opensource.org/license/mit/",
+        },
+        lifespan=dbengine_lifespan,
+        openapi_url="/api/v1/openapi.json",
+        docs_url="/api",
+    )
+
+
+    app.state.settings = get_settings()
+
+
+    @app.middleware("http")
+    async def add_db_session_middleware(request: fastapi.Request, call_next):
+        with get_dbsession(request.app.state.dbengine) as dbsession:
+            request.state.dbsession = dbsession
+            response = await call_next(request)
+            return response
+
+
+    app.mount(
+        "/static",
+        fastapi.staticfiles.StaticFiles(directory=app.state.settings.static_dir),
+        name="static",
+    )
+
+
+    templates = fastapi.templating.Jinja2Templates(directory=app.state.settings.template_dir)
+
+
+    @app.get("/favicon.ico", include_in_schema=False)
+    async def get_favicon():
+        raise fastapi.HTTPException(status_code=404, detail="Not found")
+
+
+    @app.get("/", include_in_schema=False)
+    async def redirect_docs(request: fastapi.Request):
+        return templates.TemplateResponse(
+            "index.html",
+            {
+                "request": request,
+                "environment": app.state.settings.environment,
+                "version": __version__
+            }
+        )
+
+    @app.get("/_{page:path}", include_in_schema=False)
+    async def human_pages(request: fastapi.Request, page:str):
+        return templates.TemplateResponse(
+            page,
+            {
+                "request": request,
+                "environment": app.state.settings.environment,
+                "version": __version__
+            }
+        )
+
+    app.include_router(
+        rslv.routers.resolver.router,
+    )
+
+    return app
+
+app = create_application()
--- n2t/config.py
+++ n2t/config.py
@@ -1,3 +1,4 @@
+import functools
 import os.path
 import typing
 
@@ -9,14 +10,20 @@ class Settings(pydantic_settings.BaseSettings):
     # env_prefix provides the environment variable prefix
     # for overriding these settings with env vars.
     # e.g. RSLV_PORT=11000
-    model_config = pydantic_settings.SettingsConfigDict(env_prefix="rslv_")
+    model_config = pydantic_settings.SettingsConfigDict(env_prefix="rslv_", env_file=".env")
     host: str = "localhost"
     port: int = 8000
     protocol: str = "http"
-    db_connection_string: str = f"sqlite:///{BASE_FOLDER}/data/pid_config.sqlite"
+    db_connection_string: str = f"sqlite:///{BASE_FOLDER}/data/n2t.sqlite"
+    json_dir: str = f"{BASE_FOLDER}/prefixes"
     static_dir: str = os.path.join(BASE_FOLDER, "static")
     template_dir: str = os.path.join(BASE_FOLDER, "templates")
     log_filename: typing.Optional[str] = None
+    environment: str = "development"
 
 
-settings = Settings()
+@functools.lru_cache
+def get_settings(env_file=None):
+    if env_file is None:
+        return Settings()
+    return Settings(_env_file=env_file)
