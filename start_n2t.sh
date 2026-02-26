e51b4ac06c7ceda5dc95c7ff8e90f4c828cc0607
jsjiang <jing.jiang@ucop.edu>
2026-02-26 12:38:13 -0800

    use /Users/jjiang for dir path

--- start_n2t.sh
+++ start_n2t.sh
@@ -1,6 +1,6 @@
 brew services restart nginx 
 
-export N2T_SETTINGS=/Users/jjiang/my_dev_space/n2t/dev-config.env
+export N2T_SETTINGS=/$HOME/my_dev_space/n2t/dev-config.env
 
 .venv/bin/python -m gunicorn n2t.app:app \
   -k uvicorn.workers.UvicornWorker \
