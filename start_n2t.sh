b8bb88c409a6f1ba529abab3e3046647a2c6a558
jsjiang <jing.jiang@ucop.edu>
2026-03-02 12:42:38 -0800

    config access log

--- start_n2t.sh
+++ start_n2t.sh
@@ -1,4 +1,13 @@
-brew services restart nginx 
+#!/bin/bash
+
+# start nginx using default config file, which is located at /opt/homebrew/etc/nginx/nginx.conf
+# brew commands:
+brew services stop nginx 
+brew services start nginx 
+
+# stop nginx if it's already running
+#nginx -s stop
+#nginx
 
 export N2T_SETTINGS=/$HOME/my_dev_space/n2t/dev-config.env
 
