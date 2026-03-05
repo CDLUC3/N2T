ccc2788d202ea0fac07d47b9dbd997ab4e78e994
jsjiang <jing.jiang@ucop.edu>
2026-03-05 13:28:37 -0800

    update setting path

--- start_n2t.sh
+++ start_n2t.sh
@@ -12,7 +12,7 @@ brew services start nginx
 # load schemes into the database
 python n2t -c dev-config.env loaddb
 
-export N2T_SETTINGS=/$HOME/my_dev_space/n2t/dev-config.env
+export N2T_SETTINGS=$HOME/my_dev_space/n2t/dev-config.env
 
 .venv/bin/python -m gunicorn n2t.app:app \
   -k uvicorn.workers.UvicornWorker \
